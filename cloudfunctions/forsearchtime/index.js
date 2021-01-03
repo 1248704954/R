b3b7661f8f87bfec1d9714d8d38f2ae963641960
unknown <unknown@example.com>
2021-01-03 10:55:33 +0800

    测试
    测试

---
+++ cloudfunctions/forsearchtime/index.js
@@ -0,0 +1,25 @@
+const cloud = require('wx-server-sdk')
+cloud.init({env: 'mrchen-9gvs0g3k54e7dcb7'})
+const db = cloud.database()
+const _ = db.command
+const $ = _.aggregate
+
+exports.main = async (event, context) => {
+  return await db.collection('Course_Material').aggregate().match({
+    Course_Id: event.courseID
+ }).project({
+    // filedate: 1
+    _id: 0,
+    file:1,
+    filename:1,
+    formatDate: $.dateToString({
+      date:'$filedate',
+      format: '%Y-%m-%d',
+      timezone: 'Asia/Shanghai',
+      onNull:'null'
+    })
+  }).end()
+
+// exports.main = async (event, context) => {
+//  return 
+}
